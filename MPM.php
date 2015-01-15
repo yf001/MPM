2681be75e5cff7910f28f7a8a60eae234ab942e1
yf001 <yf001web@gmx.com>
2015-01-15 12:55:22 +0900

    Initial commit

---
+++ MPM.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * MPM Library
+ * 
+ * Copyright (c) 2015 yf001
+ * 
+ * This software is released under the MIT License.
+ * 
+ * http://opensource.org/licenses/mit-license.php
+*/
+use pocketmine\Server;
+
+class MPM{
+
+	private static $API = 1.0.0;
+	private static $obj = null;
+	private static $MoneyPlugin = null;
+	
+	public function __construct(){
+		if(!self::$obj instanceof MPM){
+			self::$obj = $this;
+		}
+	}
+
+	public static function init(){
+		if(!self::$obj instanceof MPM){
+			self::$obj = new self;
+		}
+	}
+	
+	public static function getInstance(){
+		if(!self::$obj instanceof MPM){
+			self::$obj = new self;
+		}
+		return self::$obj;
+	}
+	
+	public function moneyPluginCheck(){
+		if(Server::getInstance()->getPluginManager()->getPlugin("PocketMoney") !== null){
+			$this->PocketMoney = Server::getInstance()->getPluginManager()->getPlugin("PocketMoney");
+			self::MoneyPlugin = $this->PocketMoney;
+			return true;
+		}elseif($this->getServer()->getPluginManager()->getPlugin("EconomyAPI") !== null){
+			$this->EconomyS = Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI");
+			self::MoneyPlugin = $this->EconomyS;
+		}elseif(Server::getInstance()->getPluginManager()->getPlugin("MassiveEconomy") !== null){
+			$this->ME = Server::getInstance()->getPluginManager()->getPlugin("MassiveEconomy");
+			self::MoneyPlugin = $this->ME;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * @param int $money
+	 *
+	 * @return string
+	 */
+	public function moneychange($money){
+		if(isset($this->PocketMoney)){
+			return $money . "mp";
+		}elseif(isset($this->EconomyS)) {
+			return "$" . $money;
+		}elseif(isset($this->ME)) {
+			return $this->ME->getMoneySymbol() . $money;
+		}
+	}
+	/**
+	 * @param string $player
+	 *
+	 * @return boolean
+	 */
+	public function accountExists($player){
+		if(isset($this->PocketMoney)){
+			return true;
+		}elseif(isset($this->EconomyS)) {
+			return $this->EconomyS->accountExists($player);
+		}elseif(isset($this->ME)) {
+			return $this->ME->isPlayerRegistered($player);
+		}
+	}
+	
+	/**
+	 * @param string $player
+	 * @param int $amount
+	 */
+	public function addMoney($player,$amount){
+		if(isset($this->PocketMoney)){
+			$this->PocketMoney->grantMoney($player,$amount);
+		}elseif(isset($this->EconomyS)) {
+			$this->EconomyS->addMoney($player,$amount);
+		}elseif(isset($this->ME)) {
+			$this->ME->payPlayer($player,$amount);
+		}
+	}
+	
+	/**
+	 * @param string $player
+	 * 
+	 * @return int
+	 */
+	public function getMoney($player){
+		if(isset($this->PocketMoney)){
+			return $this->PocketMoney->getMoney($player);
+		}elseif(isset($this->EconomyS)) {
+			return $this->EconomyS->myMoney($player);
+		}elseif(isset($this->ME)) {
+			return $this->ME->myMoney($player);
+		}
+	}
+	
+	/**
+	 * @param string $player
+	 * @param int $amount
+	 */
+	public function setMoney($player,$amount){
+		if(isset($this->PocketMoney)){
+			$this->PocketMoney->setMoney($player,$amount);
+		}elseif(isset($this->EconomyS)) {
+			$this->EconomyS->setMoney($player,$amount);
+		}elseif(isset($this->ME)) {
+			$this->ME->setMoney($player,$amount);
+		}
+	}
+	
+	/**
+	 * @param string $sender
+	 * @param int $player
+	 * @param int $amount
+	 */
+	public function payMoneyToPlayer($sender,$player,$amount){
+		if(isset($this->PocketMoney)){
+			$this->PocketMoney->payMoney($sender,$player,$amount);
+		}elseif(isset($this->EconomyS)) {
+			$this->EconomyS->reduceMoney($sender,$amount);
+			$this->EconomyS->addMoney($player, $amount);
+		}elseif(isset($this->ME)) {
+			$this->ME->payMoneyToPlayer($sender,$amount,$player);
+		}
+	}
+	
+	/**
+	 * @param string $player
+	 * @param int $amount
+	 */
+	public function reduceMoney($player,$amount){
+		if(isset($this->PocketMoney)){
+			$money = $this->PocketMoney->getMoney($player) - $amount;
+			$this->PocketMoney->setMoney($player,$money);
+		}elseif(isset($this->EconomyS)) {
+			$this->EconomyS->takeDebt($player,$amount);
+		}elseif(isset($this->ME)) {
+			$this->ME->takeMoney($player,$amount,$player);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # MoneyPluginManagement
-Library
+
+マネープラグインの違いを気にせず使えるようにするライブラリ?です。<br/>
+
+#API
+
+まず以下のコードで読み込んでください<br/>
+```php
+use MPM\MPM;
+```
+最初に以下のコードも実行してください<br/>
+```php
+MPM::getInstance->init();
+MPM::getInstance->moneyPluginCheck();
+```
+
+moneyPluginCheck()はbooledが返ってきます。<br/>
+falseの場合は対応しているプラグインがありません。<br/>
+falseが返ってきた場合はシャットダウンなどをおこなってください。<br/>
+
+関数を呼び出したいときは<br/>
+```php
+MPM::getInstance->(関数名);
+```
+
+##サンプルコード
+```php
+use MPM\MPM;
+
+$this->getLogger()->info("マネープラグインをチェックしています...");
+if(MPM::getInstance->moneyPluginCheck()){
+	$this->getLogger()->info(MPM::getInstance->MoneyPlugin->getName() . "を検出しました。");
+}else{
+	$this->getLogger()->info(TextFormat::RED."対応しているマネープラグインが見つかりませんでした!");
+	$this->getLogger()->info(TextFormat::RED."サーバーを停止します...");
+	$this->getServer()->shutdown();
+}
+```
